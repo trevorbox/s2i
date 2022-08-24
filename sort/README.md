7f907ffa35762d9617ee3f98f2e49ada5a5e711f
trevorbox <trevorjbox@gmail.com>
2022-08-23 17:56:26 -0700

    Dockerfile

---
+++ sort/README.md
@@ -0,0 +1,6 @@
+# example semver util
+
+```sh
+podman build -t test .
+podman run -it test /bin/bash -c "/opt/app-root/gobinary --repository=quay.io/trevorbox/pipeline-test-go --release=1.2.1"
+```
--- sort/main.go
+++ sort/main.go
@@ -6,9 +6,12 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"encoding/json"
+
+	"flag"
 
 	"github.com/coreos/go-semver/semver"
 )
@@ -19,13 +22,30 @@ type ListTags struct {
 }
 
 func main() {
-	//"docker://quay.io/trevorbox/pipeline-test-go"
-	cmd := exec.Command("skopeo", "list-tags", os.Args[1])
+	var releaseFlag = flag.String("release", "0.1.0", "the Major.Minor.Patch release semver to create a new preRelease version from")
+	var repositoryFlag = flag.String("repository", "", "the image repository, for example quay.io/trevorbox/pipeline-test-go")
+
+	flag.Parse()
+
+	release, err := semver.NewVersion(*releaseFlag)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	var newestRelevantSemver = semver.Version{
+		Major:      release.Major,
+		Minor:      release.Minor,
+		Patch:      release.Patch,
+		PreRelease: semver.PreRelease("0"),
+	}
+
+	cmd := exec.Command("skopeo", "list-tags", "docker://"+*repositoryFlag)
 	cmdOutput := &bytes.Buffer{}
 	cmdErr := &bytes.Buffer{}
 	cmd.Stdout = cmdOutput
 	cmd.Stderr = cmdErr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		log.Println(cmdErr)
 		os.Exit(1)
@@ -34,28 +54,35 @@ func main() {
 	var arr ListTags
 	_ = json.Unmarshal(cmdOutput.Bytes(), &arr)
 
-	fmt.Printf("Unsorted Tags: %s\n", arr.Tags)
-	var tags []*semver.Version
-	for _, tag := range arr.Tags {
+	newestRelevantSemver.PreRelease = semver.PreRelease(strconv.FormatInt(determineNextPreRelease(arr.Tags, release), 10))
+	fmt.Printf("%s\n", newestRelevantSemver.String())
+	os.Exit(0)
+}
 
-		t := strings.TrimPrefix(tag, "v")
+func determineNextPreRelease(tags []string, release *semver.Version) int64 {
+	var newestPreRelease int64 = -1
+	for _, tag := range tags {
 
-		semverTag, err := semver.NewVersion(t)
+		semverTag, err := semver.NewVersion(strings.TrimPrefix(tag, "v"))
 
 		if err != nil {
-			log.Println(err)
+			// it is not a valid semver
+			continue
 		}
+
 		if semverTag != nil {
-			tags = append(tags, semverTag)
+			// tags = append(tags, semverTag)
+			if semverTag.Major == release.Major && semverTag.Minor == release.Minor && semverTag.Patch == release.Patch {
+				preRelease, err := strconv.ParseInt(string(semverTag.PreRelease), 10, 64)
+				if err == nil {
+					if preRelease > newestPreRelease {
+						newestPreRelease = preRelease
+					}
+				}
+			}
 		}
 	}
-	semver.Sort(tags)
-	fmt.Printf("Sorted Tags: %s\n", tags)
 
-	newest := tags[len(tags)-1]
-	fmt.Printf("Most Recent: %s\n", newest.String())
-
-	newest.BumpPatch()
-	fmt.Printf("Next: v%s\n", newest.String())
-
+	newestPreRelease++
+	return newestPreRelease
 }
