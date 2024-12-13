9f2c99281f75e91a5e0f2b2f4e2273a781a79711
Trevor Box <trevorjbox@gmail.com>
2024-12-13 13:30:31 -0600

    update to allow arbitrary headers returned from request body

--- go/main.go
+++ go/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 const defaultHeaders = "{\"headers\":[{\"key\":\"X-Powered-By\",\"value\":\"Go\"},{\"key\":\"X-XSS-Protection\",\"value\":\"0\"},{\"key\":\"X-Content-Type-Options\",\"value\":\"\"},{\"key\":\"Set-Cookie\",\"value\":\"id=a3fWa; Max-Age=2592000\"},{\"key\":\"Set-Cookie\",\"value\":\"id=b3fWa; Max-Age=3592000\"}]}"
+const USAGE = "Usage: Set the RESPONSE_HEADERS environment variable to always return custom response headers for a GET request, else static default ones will be returned. Alternatively, send a POST or PUT request with the headers you want returned.\nExample: curl -i -X POST localhost:8080 -d '{\"headers\":[{\"key\":\"k1\",\"value\":\"v2\"}]}'"
+const ENV_VAR_RESPONSE_HEADERS = "RESPONSE_HEADERS"
 
 type Headers struct {
 	Headers []KV
@@ -20,24 +23,54 @@ type KV struct {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := os.Getenv("RESPONSE")
-	if len(response) == 0 {
-		response = "Hello OpenShift!"
-	}
 
-	responseHeaders := os.Getenv("RESPONSE_HEADERS")
-	if len(responseHeaders) == 0 {
-		responseHeaders = defaultHeaders
-	}
+	var response string
+	var errorMsg string
+
+	httpResponseCode := http.StatusOK
 
 	headers := &Headers{}
-	if err := json.Unmarshal([]byte(responseHeaders), &headers); err != nil {
-		panic(err)
+	if r.Method == http.MethodPost || r.Method == http.MethodPut {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			httpResponseCode = http.StatusInternalServerError
+			errorMsg = err.Error()
+		} else {
+			if len(body) > 0 {
+				if err := json.Unmarshal(body, &headers); err != nil {
+					httpResponseCode = http.StatusBadRequest
+					errorMsg = err.Error()
+				} else {
+					response = "Returned headers from POST or PUT request body."
+				}
+			}
+		}
 	}
+	if len(headers.Headers) == 0 {
+		responseHeaders := os.Getenv(ENV_VAR_RESPONSE_HEADERS)
+		if len(responseHeaders) == 0 {
+			responseHeaders = defaultHeaders
+			response = "Returned hard coded default headers."
+		} else {
+			response = fmt.Sprintf("Returned headers from the %s environment variable.", ENV_VAR_RESPONSE_HEADERS)
+		}
+		if err := json.Unmarshal([]byte(responseHeaders), &headers); err != nil {
+			httpResponseCode = http.StatusInternalServerError
+			errorMsg = err.Error()
+		}
+	}
+
+	if len(errorMsg) > 0 {
+		response = fmt.Sprintf("%s\nError: %s", response, errorMsg)
+	}
+
+	response = fmt.Sprintf("%s\n%s", response, USAGE)
 
 	for _, header := range headers.Headers {
 		w.Header().Add(header.Key, header.Value)
 	}
+
+	w.WriteHeader(httpResponseCode)
 
 	fmt.Fprintln(w, response)
 	fmt.Println("Servicing request.")
