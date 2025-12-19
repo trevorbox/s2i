99974fb23db4f5c100618d3b160068ae28dc116e
trevorbox <trevorjbox@gmail.com>
2025-12-19 15:54:58 -0700

    update no Symlink

--- httpd/charts/httpd/scripts/run.sh
+++ httpd/charts/httpd/scripts/run.sh
@@ -3,7 +3,12 @@
 set -x
 
 # this is important
-rm -f /etc/httpd/conf.d/welcome.conf
+mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.SAVE
+echo "" > /etc/httpd/conf.d/welcome.conf
+mkdir -p /tmp/src
+cp -r /sources/* /tmp/src/
+# Need at least an index.html to prevent httpd from complaining if Indexing disabled
+# touch /tmp/src/index/index.html
 
 /usr/libexec/s2i/assemble
 
